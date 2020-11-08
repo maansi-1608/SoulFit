bcd8cc7ea590527c4bb5a53e333b6880e4025f69
ranga1201 <ranganyaki.venkatraman@gmail.com>
2020-11-08 15:32:37 +0530

    Caretaker files added

---
+++ lib/mapnav.dart
@@ -0,0 +1,24 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
+import 'package:flutter/services.dart';
+import 'dart:typed_data';
+
+class MyMap extends StatefulWidget {
+
+  @override
+  _MyMapState createState() => _MyMapState();
+}
+
+
+class _MyMapState extends State<MyMap> {
+
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.black,
+    );
+  }
+
+}
+
